6b7a5b3135b348f23eaa77605738093ea38249fc
Sanjay <sanjay.ray@outlook.com>
2018-06-11 17:27:56 +0530

    ExtentReporterNG.java for HTML report has added

---
+++ src/main/java/com/smava/utilities/Listeners.java
@@ -0,0 +1,48 @@
+package com.smava.utilities;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.internal.annotations.IListeners;
+
+@SuppressWarnings("unused")
+public class Listeners implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println(result.getName() + " execution is completed.");
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println(result.getName()+ " execution is completed. ");
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
